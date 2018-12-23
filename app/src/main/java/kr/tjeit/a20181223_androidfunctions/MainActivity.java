44d18e2075e193f7055c5d556fd8974053c0e6f0
Hottory <gkffndnl123@naver.com>
2018-12-23 12:25:15 +0900

    얼럿다이얼로그 예시

--- app/src/main/java/kr/tjeit/a20181223_androidfunctions/MainActivity.java
+++ app/src/main/java/kr/tjeit/a20181223_androidfunctions/MainActivity.java
@@ -1,13 +1,21 @@
 package kr.tjeit.a20181223_androidfunctions;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bindViews();
         setupEvents();
@@ -18,7 +26,34 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-        
+
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                경고창을 띄우고, 확인을 누르면 로그아웃 되었다는 토스트
+//                취소를 누르면 아무일도 없던걸으로 처리.
+
+                AlertDialog.Builder alert= new AlertDialog.Builder(mContext);
+                alert.setTitle("로그아웃 확인");
+                alert.setMessage("정말 로그아웃 하시겠습니까?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Toast.makeText(mContext, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+                alert.setNegativeButton("취소", null);
+                alert.show();
+
+            }
+        });
+
+
     }
 
     @Override
@@ -28,6 +63,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+
+        this.logoutBtn = (Button) findViewById(R.id.logoutBtn);
 
     }
 }
