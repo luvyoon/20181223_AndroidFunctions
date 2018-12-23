90f1f0a775b460ee5148a63c7d00e7f8b80ccd45
Hottory <gkffndnl123@naver.com>
2018-12-23 12:40:41 +0900

    timePickerDialog 예시

--- app/src/main/java/kr/tjeit/a20181223_androidfunctions/MainActivity.java
+++ app/src/main/java/kr/tjeit/a20181223_androidfunctions/MainActivity.java
@@ -1,15 +1,20 @@
 package kr.tjeit.a20181223_androidfunctions;
 
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
     private android.widget.Button logoutBtn;
+    private android.widget.TextView timeTxt;
+    private Button timePickerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +40,7 @@ public class MainActivity extends BaseActivity {
 //                경고창을 띄우고, 확인을 누르면 로그아웃 되었다는 토스트
 //                취소를 누르면 아무일도 없던걸으로 처리.
 
-                AlertDialog.Builder alert= new AlertDialog.Builder(mContext);
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
                 alert.setTitle("로그아웃 확인");
                 alert.setMessage("정말 로그아웃 하시겠습니까?");
                 alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
@@ -54,6 +59,30 @@ public class MainActivity extends BaseActivity {
         });
 
 
+        timePickerBtn.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View v) {
+
+                                                 TimePickerDialog.OnTimeSetListener otsl = new TimePickerDialog.OnTimeSetListener() {
+                                                     @Override
+                                                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                                         String selectedTime = String.format("%d시 %d분", hourOfDay, minute);
+
+                                                         timeTxt.setText(selectedTime);
+
+
+
+
+                                                     }
+                                                 };
+                                                 TimePickerDialog tpd = new TimePickerDialog(mContext,otsl, 13, 30, false);
+                                                 tpd.show();
+                }
+          }
+        );
+
+
     }
 
     @Override
@@ -64,6 +93,9 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+
+        this.timePickerBtn = (Button) findViewById(R.id.timePickerBtn);
+        this.timeTxt = (TextView) findViewById(R.id.timeTxt);
         this.logoutBtn = (Button) findViewById(R.id.logoutBtn);
 
     }
